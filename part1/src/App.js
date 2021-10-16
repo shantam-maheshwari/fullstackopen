60e22c3466a4873d34f93157a41f02201c92f6a4
Shantam Maheshwari <shantam.maheshwari@learner.manipal.edu>
2021-10-16 13:25:22 +0530

    part1: example 6 (multiple props)

--- part1/src/App.js
+++ part1/src/App.js
@@ -3,17 +3,22 @@ import React from "react";
 const Hello = (props) => {
   return (
     <div>
-      <p>Hello {props.name}</p>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
   );
 };
 
 const App = () => {
+  const name = "Shantam";
+  const age = 22;
+
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello name="Shantam" />
-      <Hello name="Maheshwari" />
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   );
 };
