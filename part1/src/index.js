0af6932ecd27e764ac9b5193058b64d1f50bfc47
Shantam Maheshwari <shantam.maheshwari@learner.manipal.edu>
2022-01-14 02:46:42 +0530

    part1: example 8 (Page re-rendering)

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,4 +1,13 @@
 import ReactDOM from "react-dom";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+let counter = 1;
+
+const refresh = () => {
+  ReactDOM.render(<App counter={counter} />, document.getElementById("root"));
+};
+
+setInterval(() => {
+  refresh();
+  counter += 1;
+}, 1000);
